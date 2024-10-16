4925c94b9b18e4724305359bee38ed1ab161b2e6
GusGiam01 <95728668+GusGiam01@users.noreply.github.com>
2024-10-16 20:56:06 -0300

    Delete Front-End/HTML&CSS-SinAngular directory

--- Front-End/HTML&CSS-SinAngular/js/flipCard.js
+++
@@ -1,7 +0,0 @@
-const cards = document.querySelectorAll('.card__inner');
-
-cards.forEach(function(card) {
-    card.addEventListener('click', function () {
-        card.classList.toggle('is-flipped');
-    });
-});
