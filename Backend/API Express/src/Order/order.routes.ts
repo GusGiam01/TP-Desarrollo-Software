1189a3c7977ab846b904c122379751ff8d31b329
GusGiam01 <gustgiam2001@gmail.com>
2024-11-14 15:31:06 -0300

    agregando arreglo orders a user

--- Backend/API Express/src/Order/order.routes.ts
+++ Backend/API Express/src/Order/order.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { sanitizeOrderInput, findAll, findOne, findOneByUserId, add, update, remove } from "./order.controller.js";
+import { sanitizeOrderInput, findAll, findOne, findAllByUserId, add, update, remove } from "./order.controller.js";
 
 export const orderRouter = Router()
 
 orderRouter.get('/', findAll)
-orderRouter.get('/:id', findOne)
-orderRouter.get('/:user', findOneByUserId)
+//orderRouter.get('/:id', findOne)
+orderRouter.get('/:user', findAllByUserId)
 orderRouter.post('/', sanitizeOrderInput, add)
 orderRouter.put('/:id', sanitizeOrderInput, update)
 orderRouter.patch('/:id', sanitizeOrderInput, update)
