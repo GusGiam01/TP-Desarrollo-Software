0a683d0f3284f49039e31d559590eca484a05f9a
Juan Cruz Mondino <juancm.2000@hotmail.com>
2026-01-24 17:30:56 -0300

    Dejo las opciones de coneccion del orm para mongoDB, tanto local como en la nube.

--- Backend/API Express/src/shared/db/orm.ts
+++ Backend/API Express/src/shared/db/orm.ts
@@ -7,6 +7,7 @@ export const orm = await MikroORM.init({
   dbName: 'tiendaSM',
   type: "mongo",
   //clientUrl: 'mongodb+srv://usuario:usuario@cluster.k0o09.mongodb.net/',
+  //clientUrl: 'mongodb://localhost:27017/tiendaSM',
   clientUrl: 'mongodb://admin:admin123@localhost:27017/tiendaSM?authSource=admin',
   highlighter: new MongoHighlighter(),
   debug: true,
